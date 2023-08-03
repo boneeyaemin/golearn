623c0218148aaaf99da066effadd37da11401309
boneeyeamin <boneeyeamin21@gmail.com>
2023-08-03 10:16:55 +0600

    new section added

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,15 @@ $(document).ready(function(){
         });
 
 
-
+        $('.video-icon').magnificPopup({
+            disableOn: 700,
+            type: 'iframe',
+            mainClass: 'mfp-fade',
+            removalDelay: 160,
+            preloader: false,
+    
+            fixedContentPos: false
+        });
 
 
 });

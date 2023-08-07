b4defe020553fcc013094e052fdb2474b0c3e27a
boneeyeamin <boneeyeamin21@gmail.com>
2023-08-08 00:22:50 +0600

    n

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,5 @@
 $(document).ready(function(){
     
-        // $('.h-menu ul').clone().appendTo('.mobile-menu');
-        // $('.menu-trigger').on("click", function(){
-        //     $('.mobile-menu').slideToggle();
-        //     console.log( "it works!" );
-            
-        // });
-
         // play button
         $('.video-icon').magnificPopup({
             disableOn: 700,
@@ -24,7 +17,7 @@ $(document).ready(function(){
             items:3,
             nav:true,
             dots:true,
-            autoplay: false,
+            autoplay: true,
             margin:15,
             loop: true,
             smartSpeed: 600,
@@ -32,7 +25,7 @@ $(document).ready(function(){
             responsive:{
                 0:{
                     items:1,
-                    nav:true
+                    nav:false
                 },
                 767:{
                     items:2,
@@ -47,7 +40,7 @@ $(document).ready(function(){
             navText: [
                 '<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" /></svg>',
                 '<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" /></svg>'
-            ]
+            ],
         });
 
 
@@ -82,29 +75,7 @@ $(document).ready(function(){
             },
         });
 
-        // nothing
-
-            $('#filterOptions li a').click(function() {
-              // fetch the class of the clicked item
-              var ourClass = $(this).attr('class');
-          
-              // reset the active class on all the buttons
-              $('#filterOptions li').removeClass('active');
-              // update the active state on our clicked button
-              $(this).parent().addClass('active');
-          
-              if(ourClass == 'all') {
-                // show all our items
-                $('#ourHolder').children('div.item').show();
-              }
-              else {
-                // hide all elements that don't share ourClass
-                $('#ourHolder').children('div:not(.' + ourClass + ')').hide();
-                // show all elements that do share ourClass
-                $('#ourHolder').children('div.' + ourClass).show();
-              }
-              return false;
-            });
+       
 
     $('.filter-button li').on("click",function(){
         var button = $(this).data('filter');
